0f88490001ecdeee74d109952881b5fce385e6ba
Gomex <linux.rafa@gmail.com>
2020-08-29 14:29:44 -0300

    Estava no repositório errado

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ data "aws_ami" "ubuntu" {
     values = ["IaaSWeek-${var.hash_commit}"]
   }
 
-  owners = ["777015859311"] # My User
+  owners = ["178520105998"] # My User
 }
 
 resource "aws_instance" "web" {
@@ -17,5 +17,3 @@ resource "aws_instance" "web" {
     Name = var.app_name
   }
 }
-
-
--- variable.tf
+++ variable.tf
@@ -2,14 +2,14 @@ variable "hash_commit" {
   default = "806d52dafe9b7fddbc4f0d2d41086ed3cfa02a44"
 }
 
-variable "instance_type" {
-  type        = string
-  default     = "t3.micro"
-  description = "Instance type"
-}
-
 variable "app_name" {
-  type = string
-  description = "Sem nome"
+  type        = string
+  default     = "Sem nome"
+  description = "Nome da app"
 }
 
+variable "instance_type" {
+  type = string
+  default = "t3.micro"
+  description = "Tipo de instância ec2 usada na AWS"
+}
